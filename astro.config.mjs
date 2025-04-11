c8ee75f6b616a57d77bde9b6ac9c5b9ff81f1c11
Ludovic Laffineur <ludovic.laffineur@gmail.com>
2025-04-11 21:49:34 +0200

    impr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   site: 'https://foxyline.ai',
-  
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'fr', 'nl'],
+    routing: {
+      prefixDefaultLocale: true
+    }
+  }
 });
